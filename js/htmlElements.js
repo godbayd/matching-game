8500690a8dc03648e490fc1c1c24251c09344e48
morpheus <godbayd@aol.com>
2019-08-13 13:12:53 -0400

    In the html, added .board inside #game. Moved cells to board and updated js and css according to changes

--- js/htmlElements.js
+++ js/htmlElements.js
@@ -1,6 +1,6 @@
 import {select, selectAll} from './utils'
 
 // html element arrays
-export const hiddenNumberArr   = selectAll('#game .cell .hidden-number-container')
-export const cellsArr    = selectAll('#game .cell')
-export const overlaysArr = selectAll('#game .cell .overlay')
+export const hiddenNumberArr   = selectAll('#game .board .cell .hidden-number-container')
+export const cellsArr    = selectAll('#game .board .cell')
+export const overlaysArr = selectAll('#game .board .cell .overlay')

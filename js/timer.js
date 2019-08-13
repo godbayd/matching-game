fe3d97cc312bc0ddf9005ba3a547a62ad8308f9d
morpheus <godbayd@aol.com>
2019-08-13 16:37:35 -0400

    unformatted timer is logging correct time

--- js/timer.js
+++ js/timer.js
@@ -3,11 +3,62 @@ import {startTimerBtn, minutesDiv, secondsDiv} from './htmlElements'
 
 
 const start = Date.now();
+let timeState = {
+    minutes: 2,
+    seconds: 60,
+    get: function(key){
+        return this[key]
+    },
+    set: function(key, val) {
+        this[key] = val
+    },
+    decrementOne: function(unitOfTime) {
+        this.set(unitOfTime, this.get(unitOfTime) - 1)
+    }
+}
+
+
+/*
+    for timer state:
+        if seconds is below 1
+        - set seconds to 60
+        - subtract one from minute
+
+    for time display in html
+        if seconds is 60
+        - output 00
+*/
+
+//(60 * 3/*# of minutes*/)
+/*
+    how clocks count
+    on minute - 1:00
+    then      - 0:59 until 0:00
+*/
+
+if (timeState.get('seconds') === 60) timeState.decrementOne('minutes');
 
 setInterval(() => {
-    const delta = Date.now() - start;
-    // console.log(Math.floor(delta / 1000))
+
+    const delta = Date.now() - start,
+          secondsElapsed = Math.floor(delta / 1000)
+
+    timeState.decrementOne('seconds')
+
+    const minutesState = timeState.get('minutes'),
+          secondsState = timeState.get('seconds')
+
+    if (secondsState < 1) {
+        timeState.decrementOne('minutes')
+        timeState.set('seconds', 60);
+    }
+
+    if (secondsElapsed <= 120) {
+        console.log(minutesState + ' ' + secondsState)
+    }
+
 }, 1000)
+
 
 export const handleStartTimerBtnClick = e => {
     console.log(e)

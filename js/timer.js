e79d9a67f9e63511ca9deaff79b121e9ff817a86
morpheus <godbayd@aol.com>
2019-08-13 17:31:12 -0400

    timer logic is consolidated into timeRun function and called in handleStartTimerBtnClick callback. This starts the timer once the btn is clicked. The duration is stilled fixed at 3 because thats what it is in the timer html. App needs an initial render to set duration state and render in html accordingly so that the duration setting can be configured dynamically

--- js/timer.js
+++ js/timer.js
@@ -1,77 +1,65 @@
 import {c} from './utils'
-import {startTimerBtn, minutesDiv, secondsDiv} from './htmlElements'
+import {minutesDiv, secondsDiv} from './htmlElements'
 
 
-const start = Date.now();
-let timeState = {
-    minutes: 2,
-    seconds: 60,
-    get: function(key){
-        return this[key]
-    },
-    set: function(key, val) {
-        this[key] = val
-    },
-    decrementOne: function(unitOfTime) {
-        this.set(unitOfTime, this.get(unitOfTime) - 1)
-    }
-}
+const runTimer = (duration) => {
+    const start = Date.now();
 
-
-/*
-    for timer state:
-        if seconds is below 1
-        - set seconds to 60
-        - subtract one from minute
-
-    for time display in html
-        if seconds is 60
-        - output 00
-*/
-
-/*
-    how clocks count
-    on minute - 1:00
-    then      - 0:59 until 0:00
-*/
-
-const updateTimerHtml = (seconds, minutes) => {
-    const secStr = seconds.toString()
-    let sec = secStr.length === 1 ? '0' + secStr : seconds;
-    minutesDiv.innerHTML = minutes;
-    secondsDiv.innerHTML = sec;
-}
-
-if (timeState.get('seconds') === 60) timeState.decrementOne('minutes');
-
-setInterval(() => {
-
-    const delta = Date.now() - start,
-          secondsElapsed = Math.floor(delta / 1000)
-
-    timeState.decrementOne('seconds')
-
-    const minutesState = timeState.get('minutes'),
-          secondsState = timeState.get('seconds')
-
-    if (secondsState < 1) {
-        timeState.decrementOne('minutes')
-        timeState.set('seconds', 60);
-    }
-
-    if (secondsElapsed <= 120) {
-        updateTimerHtml(secondsState, minutesState)
-    }
-    /*
-        when timer ends
-        if (secondsElapsed === 120) {
-
+    let timeState = {
+        minutes: duration,
+        seconds: 60,
+        get: function(key){
+            return this[key]
+        },
+        set: function(key, val) {
+            this[key] = val
+        },
+        decrementOne: function(unitOfTime) {
+            this.set(unitOfTime, this.get(unitOfTime) - 1)
         }
-    */
+    }
 
-}, 1000)
+
+    const updateTimerHtml = (seconds, minutes) => {
+        const secStr = seconds.toString()
+        let sec = secStr.length === 1 ? '0' + secStr : seconds;
+        minutesDiv.innerHTML = minutes;
+        secondsDiv.innerHTML = sec;
+    }
+
+
+    if (timeState.get('seconds') === 60) timeState.decrementOne('minutes');
+
+
+    setInterval(() => {
+
+        const delta = Date.now() - start,
+              secondsElapsed = Math.floor(delta / 1000)
+
+        timeState.decrementOne('seconds')
+
+        const minutesState = timeState.get('minutes'),
+              secondsState = timeState.get('seconds')
+
+        if (secondsState < 1) {
+            timeState.decrementOne('minutes')
+            timeState.set('seconds', 60);
+        }
+
+        if (secondsElapsed <= (duration * 60)) {
+            updateTimerHtml(secondsState, minutesState)
+        }
+        /*
+            when timer ends
+            if (secondsElapsed === 120) {
+
+            }
+        */
+
+    }, 1000)
+}
 
 
 export const handleStartTimerBtnClick = e => {
-    console.log(e)
+    runTimer(3)
 }

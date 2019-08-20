805878203888dcef8b796ff887697a98040e1e88
morpheus <godbayd@aol.com>
2019-08-20 02:45:32 -0400

    show hide html function added to updateHtml.js and in handleCellClick show/hide functions are imported and used. Win case was added to handleCellClick function.

--- js/handleCellClick.js
+++ js/handleCellClick.js
@@ -1,17 +1,16 @@
 import {c} from './utils'
 import {mainState, cellsState} from './gameState'
 import {cellsArr, overlaysArr} from './htmlElements'
+import {showElem, hideElem} from './updateHtml'
 
-/*
-    export const cellsState = {
-        matchedIndexesArr: [],
-        firstClickedHiddenNumber: null,
-        indexOfFirstClicked: null
-    }
-*/
+
+const resetState = () => {
+    cellsState.firstClickedHiddenNumber = null
+    cellsState.indexOfFirstClicked = null
+}
 
 export const handleCellClick = e => {
-    
+
     const indexOfClicked = cellsArr.indexOf(e.currentTarget),
           hiddenNumberOfClicked = mainState.gameArr[indexOfClicked],
           alreadyClicked = cellsState.indexOfFirstClicked === indexOfClicked,
@@ -23,7 +22,7 @@ export const handleCellClick = e => {
         if (!cellsState.firstClickedHiddenNumber) {
             cellsState.firstClickedHiddenNumber = hiddenNumberOfClicked
             cellsState.indexOfFirstClicked = indexOfClicked
-            console.log(hiddenNumberOfClicked)
+            hideElem(overlaysArr[indexOfClicked])
         }
 
         else {
@@ -31,17 +30,17 @@ export const handleCellClick = e => {
             if (hiddenNumberOfClicked === cellsState.firstClickedHiddenNumber) {
                 cellsState.matchedIndexesArr.push(cellsState.indexOfFirstClicked)
                 cellsState.matchedIndexesArr.push(indexOfClicked)
-                cellsState.firstClickedHiddenNumber = null
-                cellsState.indexOfFirstClicked = null
-                console.log('match')
-                console.log(cellsState.matchedIndexesArr)
+                hideElem(overlaysArr[indexOfClicked])
+                if (mainState.gameArr.length === cellsState.matchedIndexesArr.length) {
+                    console.log('win')
+                }
+                resetState()
             }
             // no match case
             else {
-                cellsState.firstClickedHiddenNumber = null
-                cellsState.indexOfFirstClicked = null
-                console.log('no match')
-                console.log(cellsState.matchedIndexesArr)
+                showElem(overlaysArr[indexOfClicked])
+                showElem(overlaysArr[cellsState.indexOfFirstClicked])
+                resetState()
             }
         }
 
--- js/updateHtml.js
+++ js/updateHtml.js
@@ -6,3 +6,11 @@ import {populateHiddenNumberCons} from './populateHiddenNumberCons'
 // populate cells with hidden numbers
 export const populateCellsWithHiddenNumbers = () =>
     populateHiddenNumberCons(mainState.gameArr, hiddenNumberArr);
+
+
+// show / hide elems
+export const showElem = elem =>
+    elem.style.display = 'flex';
+
+export const hideElem = elem =>
+    elem.style.display = 'none';

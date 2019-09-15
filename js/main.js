7fc7968836e52f7748cbecc12083ff8f6fe61ec3
morpheus <godbayd@aol.com>
2019-09-15 03:16:45 -0400

    difficulty btns click listeners are added and removed accordingly

--- js/main.js
+++ js/main.js
@@ -40,17 +40,23 @@ document.body.style.height = window.innerHeight + 'px'
 // initial render
 enterStartingPage()
 
-// difficulty buttons event listener
+// initial difficulty buttons event listener
 difficultyBtnsArr.map(
     diffBtn => diffBtn.addEventListener(
         'click', handleDifficultyRadioButtonsClicks
     )
 )
 
-
 const main = e => {
 
     e.stopPropagation()
+
+    // remove difficulty buttons event listener
+    difficultyBtnsArr.map(
+        diffBtn => diffBtn.removeEventListener(
+            'click', handleDifficultyRadioButtonsClicks
+        )
+    )
 
     // game data
     mainState.gameArr = genRandomNums(1, 10, 12)
@@ -74,8 +80,18 @@ startBtnsArr.map(startBtn => startBtn.addEventListener('click', main))
 
 
 playAgainBtn.addEventListener('click', e => {
+
     e.stopPropagation()
-    console.log('clicked')
+
     TweenMax.set(innerCellsArr, {rotationX: 0})
+
+    // difficulty buttons click listeners
+    difficultyBtnsArr.map(
+        diffBtn => diffBtn.addEventListener(
+            'click', handleDifficultyRadioButtonsClicks
+        )
+    )
+
     exitWinLossBoxAnim(enterStartingPage)
+
 })

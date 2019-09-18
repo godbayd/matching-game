4789d041932bdd7098a13c9ef98cdc04f1ef4f92
morpheus <godbayd@aol.com>
2019-09-18 00:00:19 -0400

    in starting-page.js, refactored functions so that ternary operators replaced switch statements.

--- js/starting-page.js
+++ js/starting-page.js
@@ -1,7 +1,5 @@
 import {mainState} from './gameState'
-import {
-    startingPageDiv
-} from './htmlElements'
+import {startingPageDiv} from './htmlElements'
 import {
     startEasyTimer,
     startMediumTimer,
@@ -9,33 +7,16 @@ import {
 } from './timer'
 import {hideElem, intialTimeHtml} from './updateHtml'
 
-const exitPage = () => hideElem(startingPageDiv)
-
-
 /*
     radioBtn callback:
         set mainState.difficulty based on
         respective radioBtn click
 */
 export const handleDifficultyRadioButtonsClicks = e => {
-    switch (e.currentTarget.id) {
-        case 'easy-opt-btn':
-            mainState.difficulty = 1;
-            break;
-        case 'medium-opt-btn':
-            mainState.difficulty = 2;
-            break;
-        case 'hard-opt-btn':
-            mainState.difficulty = 3;
-            break;
-        default:
-            return false;
-    }
-    // const md = mainState.difficulty
-    // const diff = md === 1 ? 'easy' :
-    //              md === 2 ? 'medium' :
-    //              md === 3 ? 'hard' : false
-    // intialTimeHtml(diff)
+    const id = e.currentTarget.id
+    id === 'easy-opt-btn'   ? mainState.difficulty = 1 :
+    id === 'medium-opt-btn' ? mainState.difficulty = 2 :
+    id === 'hard-opt-btn'   ? mainState.difficulty = 3 : false
 }
 
 
@@ -43,22 +24,10 @@ export const handleDifficultyRadioButtonsClicks = e => {
     start timer based on set difficulty
 */
 export const queueTimer = e => {
-    if (mainState.difficulty) {
-        switch (mainState.difficulty) {
-            case 1:
-                startEasyTimer();
-                exitPage()
-                break;
-            case 2:
-                startMediumTimer();
-                exitPage()
-                break;
-            case 3:
-                startHardTimer();
-                exitPage()
-                break;
-            default:
-                console.log('incorrect difficulty')
-        }
+    const md = mainState.difficulty
+    if (md) {
+        md === 1 ? startEasyTimer() :
+        md === 2 ? startMediumTimer() :
+        md === 3 ? startHardTimer() : false
     }
 }
